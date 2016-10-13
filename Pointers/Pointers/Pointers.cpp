7807bb7716b177e22ff45bd00af36488cea9969e
christianwilliams20 <christianwilliams20@LAB-SCI-214-07>
2016-10-13 15:01:35 -0700

    lectureing and following along

--- Pointers/Pointers/Pointers.cpp
+++ Pointers/Pointers/Pointers.cpp
@@ -9,12 +9,41 @@ using namespace std;
 int main()
 {
 	bool repeat = true;
-	while (repeat==true){
+	while (repeat){
 		int num = -23;
 		cout << "Number of bytes: " << sizeof(num) << endl;
 		cout << "Adress of num : 0x" << &num << endl;
 		cout << "Address of num: " << int(&num) << endl;
 
+		int a = 10;
+		int b = 20;
+		int c = 30;
+
+		int*ptrA = &a;
+		int*ptrB = &b;
+		int*ptrC = &c;
+
+		int *x = new int;
+		
+		cout << *x << " Is at address " << x << endl;
+		cout << *x << " Is at address " << x << endl;
+		cout << *x << " Is at address " << x << endl;
+		cout << *x << " Is at address " << x << endl;
+		cout << *x << " Is at address " << x << endl;
+
+
+		delete x;
+
+		const int SIZE = 4;
+		int vals[SIZE] = { 4,7,11,25 };
+		int *ptr = vals;
+		for (ptr = vals; ptr < vals + SIZE; ptr++) {
+			cout << *ptr << " is stored at";
+			cout << "address " << ptr << "(" << long(ptr) << ")";
+			cout << "in memory" << endl;
+		}
+		
+		cout << " " << endl;
 		cout << "Press 1 to close" << endl;
 		int answer = 0;
 		cin >> answer;
@@ -22,7 +51,7 @@ int main()
 		{
 			repeat = false;
 		}
-		return 0;
 	}
+	return 0;
 }
 
